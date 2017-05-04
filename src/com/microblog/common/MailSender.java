dce476b7fd205831f0f706ac9f746eedab575489
daiyirui <2975332829@qq.com>
2017-05-04 10:02:03 +0800

    133

---
+++ src/com/microblog/common/MailSender.java
@@ -0,0 +1,15 @@
+package com.microblog.common;
+
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+public class MailSender {
+	
+}
